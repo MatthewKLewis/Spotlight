599b8660a51e128f58cd31127f1276ce1027c790
MatthewKLewis <matthew.knight.lewis@gmail.com>
2021-07-13 10:15:45 -0400

    new maps

--- SpotLight/src/app/eulers/eulers.component.ts
+++ SpotLight/src/app/eulers/eulers.component.ts
@@ -1,19 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DMX, SpotlightService } from './spotlight.service';
-
-const YAW_COEFF = 2.117
-const PITCH_COEFF = .705
-const COLOR_BLUE = 30;
-const COLOR_CYAN = 70;
-const COLOR_GREEN = 40;
-const COLOR_ORANGE = 50;
-const COLOR_PURPLE = 60;
-const COLOR_RED = 10;
-const COLOR_WHITE = 0;
-const COLOR_YELLOW = 20;
-
-//camera is at 495, 453
-//total image size is 834 x 769
+import { SpotlightService } from './spotlight.service';
 
 @Component({
   selector: 'app-eulers',
@@ -24,38 +10,35 @@ export class EulersComponent implements OnInit {
 
   cnv!: HTMLCanvasElement
   ctx!: CanvasRenderingContext2D
-  image: any = new Image(400,600)
+  image: any = new Image(579,503)
   chosenSpotlight:number = 0
+  coordinates: Array<string> = ['0','0']
 
   constructor(public spotLightService: SpotlightService) { }
 
   ngOnInit(): void {
     this.cnv = <HTMLCanvasElement>document.querySelector('#canvas')
     this.ctx = <CanvasRenderingContext2D>this.cnv.getContext('2d')
-
-    this.image.src = '../../assets/new_tradeshow_layout.png'
-    this.image.onload = () => { this.ctx.drawImage(this.image, 0, 0, 400, 600) }
-
-    this.cnv.width = 400
-    this.cnv.height = 600
+    this.image.src = '../../assets/office_layout.png'
+    this.image.onload = () => { this.ctx.drawImage(this.image, 0, 0, 579, 503) }
+    this.cnv.width = 579
+    this.cnv.height = 503
+    this.ctx.lineWidth = 14;
+    this.ctx.strokeStyle = 'rgba(255, 221, 83, 0.5)';  
   }
 
   canvasClick(evt: any) {
     var x = (evt.pageX - evt.originalTarget.offsetLeft);
     var y = (evt.pageY - evt.originalTarget.offsetTop);
-    
-    this.ctx.drawImage(this.image, 0, 0, 400, 600)
-    
+    this.ctx.drawImage(this.image, 0, 0, 579, 503)
     this.ctx.beginPath();
-    this.ctx.moveTo(200, 270);
+    this.ctx.moveTo(250, 175);
     this.ctx.lineTo(x, y);
     this.ctx.stroke();
 
-    var xRectified = (x / evt.target.width) * 5 //ADJUST
-    var yRectified = (y / evt.target.height) * 5 //
-
-    console.log(xRectified, yRectified)
-
+    var xRectified = (x / evt.target.width) * 5.79 //  ADJUST-
+    var yRectified = (y / evt.target.height) * 5.03 // MENT
+    this.coordinates = [xRectified.toFixed(2), yRectified.toFixed(2)];
     this.spotLightService.testSpotlight(this.chosenSpotlight,
       {
         x: xRectified,
--- SpotLight/src/app/eulers/spotlight.service.ts
+++ SpotLight/src/app/eulers/spotlight.service.ts
@@ -2,38 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface DMX {
-  x: number;
-  y: number;
-  lum: number;
-  color: number;
-  strobe: number;
-  gobo: number;
-}
-
-export interface CONFIG {
-  x: number;
-  y: number;
-  height: number;
-  spotlightOffset: string;
-  assignedTag: string;
-}
-
 @Injectable({
   providedIn: 'root',
 })
 export class SpotlightService {
   constructor(private http: HttpClient) { }
 
-  initializeSpotlight(id:number, camCon: CONFIG): Observable<any> {
-    return this.http.post(`http://localhost:5000/api/initialize/${id}`, camCon);
-  }
-
   testSpotlight(id: number, dmxObject: any): Observable<any> {
     return this.http.post(`http://localhost:5000/api/move/${id}`, dmxObject);
   }
 
-  stopSpotlight(id: number): Observable<any> {
-    return this.http.post(`http://localhost:5000/api/stop/${id}`, {});
-  }
 }
