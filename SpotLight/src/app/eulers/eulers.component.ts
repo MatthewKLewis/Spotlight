2d3174d144ef1c2721fed6e0c447a7206e027bf9
MatthewKLewis <matthew.knight.lewis@gmail.com>
2021-08-02 17:03:06 -0400

    8 2 2021

--- SpotLight/src/app/eulers/eulers.component.ts
+++ SpotLight/src/app/eulers/eulers.component.ts
@@ -10,7 +10,7 @@ export class EulersComponent implements OnInit {
 
   cnv!: HTMLCanvasElement
   ctx!: CanvasRenderingContext2D
-  image: any = new Image(579,503)
+  image: any = new Image(542,812)
   chosenSpotlight:number = 0
   coordinates: Array<string> = ['0','0']
 
@@ -19,10 +19,10 @@ export class EulersComponent implements OnInit {
   ngOnInit(): void {
     this.cnv = <HTMLCanvasElement>document.querySelector('#canvas')
     this.ctx = <CanvasRenderingContext2D>this.cnv.getContext('2d')
-    this.image.src = '../../assets/office_layout.png'
-    this.image.onload = () => { this.ctx.drawImage(this.image, 0, 0, 579, 503) }
-    this.cnv.width = 579
-    this.cnv.height = 503
+    this.image.src = '../../assets/booth.png'
+    this.image.onload = () => { this.ctx.drawImage(this.image, 0, 0, 542, 812) }
+    this.cnv.width = 542
+    this.cnv.height = 812
     this.ctx.lineWidth = 14;
     this.ctx.strokeStyle = 'rgba(255, 221, 83, 0.5)';  
   }
@@ -30,18 +30,23 @@ export class EulersComponent implements OnInit {
   canvasClick(evt: any) {
     var x = (evt.pageX - evt.originalTarget.offsetLeft);
     var y = (evt.pageY - evt.originalTarget.offsetTop);
-    this.ctx.drawImage(this.image, 0, 0, 579, 503)
+
+    //console.log(x, y)
+
+    this.ctx.drawImage(this.image, 0, 0, 542, 812)
     this.ctx.beginPath();
     if (this.chosenSpotlight == 0) {
-      this.ctx.moveTo(250, 175);
+      this.ctx.moveTo(271, 313);
     } else {
-      this.ctx.moveTo(250, 300);
+      this.ctx.moveTo(271, 498);
     }
     this.ctx.lineTo(x, y);
     this.ctx.stroke();
-
-    var xRectified = (x / evt.target.width) * 5.79 //  ADJUST-
-    var yRectified = (y / evt.target.height) * 5.03 // MENT
+    
+    var xRectified = (x / evt.target.width) * 10 //ADJUSTMENT
+    var yRectified = (y / evt.target.height) * 30
+    console.log(xRectified, yRectified)
+    
     this.coordinates = [xRectified.toFixed(2), yRectified.toFixed(2)];
     this.spotLightService.testSpotlight(this.chosenSpotlight,
       {
